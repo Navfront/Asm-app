b1c471f0d9688cdbc2c68cbd18bd0821134ff7f3
Andrey Nazhiganov <b3rt@yandex.ru>
2025-04-12 19:37:22 +0300

    Fix comments

--- code.asm
+++ code.asm
@@ -7,9 +7,10 @@ drawHorizontalLine proc
 ; R8 = opts (2b(length) + 2b(screen_width) + 2b(Y) + 2b(X))
 
 
+; Вариант цикла 1:
 ;_metka_drawHorizontalLine:
 ;
-;mov [ rcx ], rdx
+;mov [ rcx ], rdx ; чтение из регистра rcx адреса в памяти
 ;add rcx, 4
 ;dec r9
 ;jnz _metka_drawHorizontalLine
@@ -20,31 +21,31 @@ push rax
 push rcx
 
 mov eax, 0DEADBEEFh  ; Уникальное значение
-nop                  ; Для удобства
+nop                  ; Для удобства поиска
 
-; rbx = offset (width * y + x) * 4;
+; формула рассчета оффсета = (width * y + x) * 4 
 mov rbx, r8
-shr rbx, 32
-movzx rbx, bx
+shr rbx, 32 ; сдвиг до low64
+movzx rbx, bx ; чистим значение выше bx
 
 mov rax, r8
-shr rax, 16
-movzx rax, ax
+shr rax, 16 ; сдвиг до hi32
+movzx rax, ax ; чистим выше ax
 
 imul rbx, rax
 
 mov rax, r8
-movzx rax, ax
+movzx rax, ax ; чистим выше ax
 
 add rbx, rax
 
 shl rbx, 2
 
 mov rdi, rcx
-add rdi, rbx ;add offset to rdi
+add rdi, rbx ; add offset to rdi
 mov rax, rdx
 mov rcx, r8
-shr rcx, 48 ;сдвиг до length
+shr rcx, 48 ; сдвиг до length
 rep stosd
 
 pop rcx
